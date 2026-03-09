aec7341ca680fa8c4205c0369476562e10c6ac42
Adenawell Edwarlys Valentin Torres <adenawell_valentin@ucne.edu.do>
2026-03-09 19:41:18 -0400

    Creando modelos

---
+++ Models/AsignacionPuntosDetalles.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Adenawell_ValentinAP1_P2.Models;
+
+public class AsignacionPuntosDetalles
+{
+    [Key]
+
+    public int IdDetalle { get; set; }
+
+    public int IdAsignacion { get; set; }
+
+    public int CantidadPuntos {  get; set; }
+
+    public int TipoPuntoId { get; set; }
+
+    
+
+
+}
---
+++ Models/AsignacionesPuntos.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Adenawell_ValentinAP1_P2.Models;
+
+public class AsignacionesPuntos
+{
+    [Key]
+    public int IdAsignacion { get; set; }
+    public DateTime Fecha { get; set; } = DateTime.Today;
+
+    public int EstudianteId { get; set; }
+
+    public int TotalPuntos { get; set; }
+
+    public ICollection<AsignacionPuntosDetalles> ListadoDetalles { get; set; } = new List<AsignacionPuntosDetalles>();
+
+    [ForeignKey("EstudianteId")]
+    public Estudiantes? Estudiantes { get; set; }
+
+
+
+
+}
